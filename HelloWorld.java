5e81ad81bb8288470cd31022d99589663826c1af
putriayusushmita <putriayusushmita6@gmail.com>
2021-10-11 23:12:45 -0700

    commit kedua

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        String firstname, lastname, thirdname, address, makanan, hobi, phonenumber;
+        int age;
+        double tinggi;
+        
+        firstname = "Putri";
+        lastname  = "Ayu";
+        thirdname = "Sushmita" ;
+        address = "Medan" ;
+        phonenumber = "082165047861 " ;
+        age = 20;
+        tinggi = 155;
+        hobi ="membaca";
+        makanan ="ayam";
+
+        System.out.println("firstname= "+  "Putri");
+        System.out.println("lastname= "+ "Ayu");
+        System.out.println("thirdname= "+ "Sushmita");
+        System.out.println("address= " + "Medan");
+        System.out.println("phonenumber " + "082165047861");
+        System.out.println("age= " + "20");
+        System.out.println("tinggi= " + "155");
+        System.out.println("hobi= " + "membaca");
+        System.out.println("makanan= " + "ayam");
+    }
+}
